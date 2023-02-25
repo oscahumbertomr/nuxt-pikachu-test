dd4386241225bb2bbcd4d728f24ad1aa0f094d90
oscahumbertomr <oscarhumbertomr@gmail.com>
2023-02-25 14:05:54 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-pikachu-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-cpxzsk)
---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <audio controls src="sound1.mp4">
+    <a href="sound1.mp4"> Download audio </a>
+  </audio>
+  <audio controls src="sound2.mp4">
+    <a href="sound2.mp4"> Download audio </a>
+  </audio>
+</template>
