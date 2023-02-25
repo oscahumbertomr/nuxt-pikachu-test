4f5b434c5b24443ca25b00b101d13036a9eaa84c
oscahumbertomr <oscarhumbertomr@gmail.com>
2023-02-25 15:39:48 -0600

    styles added

--- app.vue
+++ app.vue
@@ -1,29 +1,56 @@
 <template>
-  <p v-on:click="audioPikachu.play">Picachu short</p>
-  <audio controls src="sound1.mp4" ref="audioPikachu" v-show="false">
-    <a href="sound1.mp4"> Download audio </a>
-  </audio>
+  <div class="main">
+    <div v-on:click="audioPikachu.play" class="button">
+      Picachu short
+      <audio controls src="sound1.mp4" ref="audioPikachu" v-show="false">
+        <a href="sound1.mp4"> Download audio </a>
+      </audio>
+    </div>
 
-  <p v-on:click="audioPikapi.play">Pika-pi</p>
-  <audio controls src="sound2.mp4" ref="audioPikapi" v-show="false">
-    <a href="sound2.mp4"> Download audio </a>
-  </audio>
+    <div v-on:click="audioPikapi.play" class="button">
+      Pika-pi
+      <audio controls src="sound2.mp4" ref="audioPikapi" v-show="false">
+        <a href="sound2.mp4"> Download audio </a>
+      </audio>
+    </div>
 
-  <p v-on:click="audioAmongUs.play">?</p>
-  <audio controls src="AmongUs.mp4" ref="audioAmongUs" v-show="false">
-    <a href="AmongUs.mp4"> Download audio </a>
-  </audio>
+    <div v-on:click="audioAmongUs.play" class="button">
+      ?
+      <audio controls src="AmongUs.mp4" ref="audioAmongUs" v-show="false">
+        <a href="AmongUs.mp4"> Download audio </a>
+      </audio>
+    </div>
+    <div v-on:click="audioGymLeader.play" class="button">
+      Gym
+      <audio controls src="GymLeader.mp4" ref="audioGymLeader">
+        <a href="GymLeader.mp4"> Download audio </a>
+      </audio>
+    </div>
 
-  <p v-on:click="audioGymLeader.play">Gym</p>
-  <audio controls src="GymLeader.mp4" ref="audioGymLeader">
-    <a href="GymLeader.mp4"> Download audio </a>
-  </audio>
-
-  <p v-on:click="audioKyaa.play">????</p>
-  <audio controls src="Kyaa.mp4" ref="audioKyaa" v-show="false">
-    <a href="GymLeader.mp4"> Download audio </a>
-  </audio>
+    <div v-on:click="audioKyaa.play" class="button">
+      ????
+      <audio controls src="Kyaa.mp4" ref="audioKyaa" v-show="false">
+        <a href="GymLeader.mp4"> Download audio </a>
+      </audio>
+    </div>
+  </div>
 </template>
+
+<style>
+.main {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100vw;
+}
+.button {
+  padding: 5em 2em;
+  margin: 1em 1em;
+  text-align: center;
+  flex: 30%;
+  border-style: solid;
+      cursor: pointer;
+}
+</style>
 
 <script setup lang="ts">
 const audioPikachu = ref();
